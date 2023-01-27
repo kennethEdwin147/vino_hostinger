e070814c7614a7935062700339ac14be62548fdf
kennethEdwin147 <edwingbeti@gmail.com>
2023-01-27 01:02:58 -0500

    pihoi

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
   plugins: [require('@tailwindcss/typography')],
 }
 
-/* npx tailwindcss -i ./public/css/input.css -o ./public/css/tailwind3.css --watch --minify
+/* npx tailwindcss -i ./public/css/input.css -o ./public/css/tailwind11.css --watch --minify
  */
